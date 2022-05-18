c7122e4d5aaabf0e8ef122e851f238b8d953ddce
rootrequired <104975826+rootrequired@users.noreply.github.com>
2022-05-18 14:07:28 +0300

    1 lab_work

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello debbuging!");
+            Console.WriteLine("Hello Read!");
+            Console.ReadLine();
+            // Я дуже радий що нарешті сижу за основним пк(16 гб оперативки(хром дуже радийXD))
+            /*Аце двох ріневий
+             коментар)*/
+
+
+        }
+    }
+}
