c2ae11d9a5b973bd033e8a602f87f16b2b7c20af
rootrequired <104975826+rootrequired@users.noreply.github.com>
2022-05-26 21:07:33 +0300

    Add files via upload

---
+++ nepriznanij genij.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_1_lab
+{
+    interface ILibraryUser
+    {
+        void AddBook(string book);
+        void RemoveBook(int index);
+        string BookInfo(int index);
+        int BooksCount();
+    }
+}
